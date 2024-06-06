43ab7d067a5f046a9d6bc34125b4d99052015c56
gurdiny <gerardour619@gmail.com>
2024-06-05 20:57:58 -0600

    Prettier

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const koderRouter = require("./routes/koders.router");
 const mentorsRouter = require("./routes/mentors.router");
 const authLog = require("./routes/auth.router");
@@ -7,6 +8,7 @@ const generationRouter = require("./routes/generation.router");
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use("/koders", koderRouter);
 app.use("/mentors", mentorsRouter);
